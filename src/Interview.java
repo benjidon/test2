78136be014d2272baca039156a9f810434f438e9
Benjamin Don <zallzool@Bens-MacBook-Pro.local>
2019-01-25 11:33:16 -0800

    Truth

--- src/Interview.java
+++ src/Interview.java
@@ -6,7 +6,7 @@ public class Interview {
 	
 	public static void main(String[] args)  {
 		
-		System.out.println("hi dd yo");
+		System.out.println("Davide is the best shooter of all time");
 		
 	}
 	
